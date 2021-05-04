87ce993a0c0b03f7ab943d3fc68e87b52f478641
Reuel O <reuenga@gmail.com>
2021-05-04 21:14:47 +0300

    initial upload

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
   session({
-    store: new pgSession({ connectionString: process.env.DATABASE_URL }),
+    store: new pgSession({ conString: process.env.DATABASE_URL }),
     secret: "secret", // encryption key
     resave: false, // resave session variables only when changes are made
     saveUninitialized: false, // do not save empty values
