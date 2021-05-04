19320551b0fafb9d3f2cd912d5db700af7d69d63
Reuel O <reuenga@gmail.com>
2021-05-04 20:31:32 +0300

    initial upload

--- config/db.js
+++ config/db.js
@@ -1,32 +1,21 @@
-const { Pool } = require("pg");
+const { Client } = require("pg");
 
-if (process.env.NODE_ENV === "production") {
-  var connectionString = {
-    host: "localhost", // 'localhost' is the default;
-    port: 5432, // 5432 is the default;
-    database: "api",
-    user: "postgres",
-    password: "reuenga7",
-  };
-  // var connectionString = "postgresql://postgres:7005@localhost:5432/api";
-} else if (process.env.NODE_ENV === "production") {
-  var connectionString = DATABASE_URL;
-}
-
-const pool = new Pool({
-  connectionString,
-  // max: 20,
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 // create tables: users, session
 const init =
   "CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, username VARCHAR UNIQUE, password VARCHAR, created TIMESTAMP); CREATE TABLE IF NOT EXISTS session(session_id VARCHAR, session_data VARCHAR, expire TIMESTAMP);";
 
-pool
+client
   .query(init)
   .then((data) => console.log(data.rows[0]))
   .catch((err) => console.error("Error executing query", err.stack));
 
-pool.connect();
+client.connect();
 
-module.exports = pool;
+module.exports = client;
